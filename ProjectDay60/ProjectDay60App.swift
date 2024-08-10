e63c6a7a821ab429c8d96538a1804b9310e25680
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-08-10 15:17:38 -0400

    initial commit

---
+++ ProjectDay60/ProjectDay60App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectDay60App.swift
+//  ProjectDay60
+//
+//  Created by Rodrigo on 10-08-24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectDay60App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

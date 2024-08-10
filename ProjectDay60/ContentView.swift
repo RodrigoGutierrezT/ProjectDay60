b4707f27825ec38cc09aa474de96929c1017502b
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-08-10 17:48:55 -0400

    feat: Implement ContentView

--- ProjectDay60/ContentView.swift
+++ ProjectDay60/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var users = [User]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(users) { user in
+                NavigationLink(value: user) {
+                    
+                    Text(user.isActive ? "🟢" : "🔴")
+                    
+                    HStack {
+                        VStack(alignment: .leading){
+                            Text(user.name)
+                            Text("company: \(user.company)")
+                        }
+                        .padding(.vertical, 5)
+                        .padding(.horizontal, 8)
+                    }
+                }
+            }
+            .navigationDestination(for: User.self) { user in
+                UserDetailView(user: user)
+            }
+            .task {
+                await loadUsers()
+            }
         }
-        .padding()
+    }
+    
+    func loadUsers() async {
+        
+        if users.count > 0 {
+            return
+        }
+        
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            print("invalid URL")
+            return
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
+        do {
+            let(data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? decoder.decode([User].self, from: data) {
+                users = decodedResponse
+            }
+            
+        } catch {
+            print("error decoding \(error)")
+        }
+        
+        
     }
 }
 
